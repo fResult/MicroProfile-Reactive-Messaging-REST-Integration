bfa01456dff58eb45538f4f81917f850af84f0fa
fResult <styxmaz@gmail.com>
2025-03-14 11:34:08 +0700

    Create `sendProperty` on `SystemService`.

--- start/system/src/main/java/io/openliberty/guides/system/SystemService.java
+++ start/system/src/main/java/io/openliberty/guides/system/SystemService.java
@@ -11,7 +11,9 @@
 // end::copyright[]
 package io.openliberty.guides.system;
 
+import io.openliberty.guides.models.PropertyMessage;
 import io.openliberty.guides.models.SystemLoad;
+import io.opentelemetry.api.internal.StringUtils;
 import io.reactivex.rxjava3.core.Flowable;
 import jakarta.enterprise.context.ApplicationScoped;
 import java.lang.management.ManagementFactory;
@@ -20,13 +22,14 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import org.reactivestreams.Publisher;
 
 @ApplicationScoped
 public class SystemService {
   private static final OperatingSystemMXBean OS_MEAN = ManagementFactory.getOperatingSystemMXBean();
-  private static Logger logger = Logger.getLogger(SystemService.class.getName());
+  private final Logger logger = Logger.getLogger(SystemService.class.getName());
   private static String hostname = null;
 
   private static String getHostname() {
@@ -43,6 +46,19 @@ public class SystemService {
   @Outgoing("systemLoad")
   public Publisher<SystemLoad> sendSystemLoad() {
     return Flowable.interval(15, TimeUnit.SECONDS)
-        .map((interval -> new SystemLoad(getHostname(), OS_MEAN.getSystemLoadAverage())));
+        .map(interval -> new SystemLoad(getHostname(), OS_MEAN.getSystemLoadAverage()));
+  }
+
+  @Incoming("propertyRequest")
+  @Outgoing("propertyResponse")
+  public PropertyMessage sendProperty(String propertyName) {
+    logger.info("sendProperty: " + propertyName);
+    if (StringUtils.isNullOrEmpty(propertyName)) {
+      logger.warning(propertyName == null ? "Null" : "An empty string" + "is not System Property");
+      return null;
+    }
+
+    return new PropertyMessage(
+        getHostname(), propertyName, System.getProperty(propertyName, "unknown"));
   }
 }
